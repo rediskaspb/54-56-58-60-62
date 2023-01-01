c0e4ac5d3086f41b55c035290bf7137221d3ff5f
Anastasiya <9399023@mail.ru>
2023-01-01 12:28:38 +0300

    add task62

--- task 62/Program.cs
+++ task 62/Program.cs
@@ -6,3 +6,36 @@
 // 12 13 14 5
 // 11 16 15 6
 // 10 9 8 7
+
+int[,] matrix = new int[4, 4];
+int i = 0;
+int j = 0;
+int step = 1;
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+while (step <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+    matrix[i, j] = step;
+    step++;
+    if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= matrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > matrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+PrintArray(matrix);
